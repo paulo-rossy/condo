6b813e0c39e5e49e2194752fe760e8bb7b6dcccd
Sitnikov Ivan <sit7602@gmail.com>
2022-02-16 14:46:10 +0500

    DOMA-2245 add xlsx download step

--- apps/condo/cypress/plugins/index.js
+++ apps/condo/cypress/plugins/index.js
@@ -1,69 +1,84 @@
 /**
  * @type {Cypress.PluginConfig}
  */
-const isEmpty = require('lodash/isEmpty')
-const { makeLoggedInAdminClient } = require('@core/keystone/test.utils')
-const {
-    createTestUser,
-    createTestForgotPasswordAction,
-    ConfirmPhoneAction,
-    makeLoggedInClient,
-} = require('@condo/domains/user/utils/testSchema')
-const { makeClientWithProperty } = require('@condo/domains/property/utils/testSchema')
-const { OrganizationEmployee } = require('@condo/domains/organization/utils/testSchema')
-const { createTestTicket } = require('@condo/domains/ticket/utils/testSchema')
+const { isFileExist, findFiles } = require('cy-verify-downloads')
+const { rmdir } = require('fs')
+// const isEmpty = require('lodash/isEmpty')
+// const { makeLoggedInAdminClient } = require('@core/keystone/test.utils')
+// const {
+//     createTestUser,
+//     createTestForgotPasswordAction,
+//     ConfirmPhoneAction,
+//     makeLoggedInClient,
+// } = require('@condo/domains/user/utils/testSchema')
+// const { makeClientWithProperty } = require('@condo/domains/property/utils/testSchema')
+// const { OrganizationEmployee } = require('@condo/domains/organization/utils/testSchema')
+// const { createTestTicket } = require('@condo/domains/ticket/utils/testSchema')
 
 let userObject = {}
 
 module.exports = async (on, config) => {
-    if (config['baseUrl'].includes('localhost')) {
-        const admin = await makeLoggedInAdminClient()
-
-        on('task', {
-            async 'keystone:createUser' () {
-                return await createTestUser(admin)
-            },
-            async 'keystone:createForgotPasswordAction' (user) {
-                return await createTestForgotPasswordAction(admin, user)
-            },
-            async 'keystone:getConfirmPhoneAction' (phone) {
-                return await ConfirmPhoneAction.getAll(admin, { phone })
-            },
-            async 'keystone:createUserWithProperty' () {
-                if (isEmpty(userObject)) {
-                    const result = await makeClientWithProperty()
-                    const client = await makeLoggedInClient(result.userAttrs)
-                    const cookie = client.getCookie()
-
-                    const organizationLink = await OrganizationEmployee.getOne(client, {
-                        user: { id: result.userAttrs.id }, isRejected: false, isBlocked: false,
-                    })
-                    const user = Object.assign({}, result.user)
-                    userObject = Object.assign({}, {
-                        user,
-                        property: result.property,
-                        cookie,
-                        organizationLinkId: organizationLink.id,
-                        userAttrs: result.userAttrs,
-                        organization: result.organization,
-                    })
-
-                    return userObject
-                }
-
-                return userObject
-
-            },
-            async 'keystone:createTickets' (ticketAttrs) {
-                const client = await makeLoggedInClient(ticketAttrs.userAttrs)
-                await createTestTicket(client, ticketAttrs.organization, ticketAttrs.property, { isWarranty: true })
-                await createTestTicket(client, ticketAttrs.organization, ticketAttrs.property, { isEmergency: true })
-                await createTestTicket(client, ticketAttrs.organization, ticketAttrs.property, { isPaid: true })
-                return null
-            },
-        })
-    }
-
+    // if (config['baseUrl'].includes('localhost')) {
+    //     const admin = await makeLoggedInAdminClient()
+    //
+    //     on('task', {
+    //         async 'keystone:createUser' () {
+    //             return await createTestUser(admin)
+    //         },
+    //         async 'keystone:createForgotPasswordAction' (user) {
+    //             return await createTestForgotPasswordAction(admin, user)
+    //         },
+    //         async 'keystone:getConfirmPhoneAction' (phone) {
+    //             return await ConfirmPhoneAction.getAll(admin, { phone })
+    //         },
+    //         async 'keystone:createUserWithProperty' () {
+    //             if (isEmpty(userObject)) {
+    //                 const result = await makeClientWithProperty()
+    //                 const client = await makeLoggedInClient(result.userAttrs)
+    //                 const cookie = client.getCookie()
+    //
+    //                 const organizationLink = await OrganizationEmployee.getOne(client, {
+    //                     user: { id: result.userAttrs.id }, isRejected: false, isBlocked: false,
+    //                 })
+    //                 const user = Object.assign({}, result.user)
+    //                 userObject = Object.assign({}, {
+    //                     user,
+    //                     property: result.property,
+    //                     cookie,
+    //                     organizationLinkId: organizationLink.id,
+    //                     userAttrs: result.userAttrs,
+    //                     organization: result.organization,
+    //                 })
+    //
+    //                 return userObject
+    //             }
+    //
+    //             return userObject
+    //
+    //         },
+    //         async 'keystone:createTickets' (ticketAttrs) {
+    //             const client = await makeLoggedInClient(ticketAttrs.userAttrs)
+    //             await createTestTicket(client, ticketAttrs.organization, ticketAttrs.property, { isWarranty: true })
+    //             await createTestTicket(client, ticketAttrs.organization, ticketAttrs.property, { isEmergency: true })
+    //             await createTestTicket(client, ticketAttrs.organization, ticketAttrs.property, { isPaid: true })
+    //             return null
+    //         },
+    //     })
+    // }
+    on('task', {
+        isFileExist,
+        findFiles,
+        deleteFolder (folderName) {
+            return new Promise((resolve, reject) => {
+                rmdir(folderName, { maxRetries: 10, recursive: true }, (err => {
+                    if (err) {
+                        return reject(err)
+                    }
+                    resolve(null)
+                }))
+            })
+        },
+    })
     return config
 
 }
