e4b086f31dbe3bcc4359ac26ba10d3326f112c2b
Sitnikov Ivan <sit7602@gmail.com>
2023-02-17 19:32:24 +0600

    fix(condo): DOMA-5139 add sbbol import modal form validations

--- apps/condo/domains/banking/components/SbbolImportModal.tsx
+++ apps/condo/domains/banking/components/SbbolImportModal.tsx
@@ -4,7 +4,7 @@ import styled from '@emotion/styled'
 import { Row, Col, Form, FormProps } from 'antd'
 import get from 'lodash/get'
 import { useRouter } from 'next/router'
-import React, { useState, useCallback, useEffect, useRef } from 'react'
+import React, { useState, useCallback, useEffect, useRef, useMemo } from 'react'
 
 import { PlusCircle, XCircle } from '@open-condo/icons'
 import { useIntl } from '@open-condo/next/intl'
@@ -12,6 +12,7 @@ import { useOrganization } from '@open-condo/next/organization'
 import { Modal, Select, Option, Alert, Button, Typography, Space } from '@open-condo/ui'
 
 import { BankAccount } from '@condo/domains/banking/utils/clientSchema'
+import { useValidations } from '@condo/domains/common/hooks/useValidations'
 import { Property } from '@condo/domains/property/utils/clientSchema'
 
 const MODAL_ROW_GUTTER: React.ComponentProps<typeof Row>['gutter'] = [16, 24]
@@ -30,6 +31,15 @@ interface ISbbolImportModal {
     ({ open }: { open: boolean }): React.ReactElement
 }
 
+const validateFormItems = (items: Array<{ property?: string, bankAccount?: string }>) => {
+    const allValuesSet = items.every(item => get(item, 'bankAccount', false) && get(item, 'property', false))
+    const bankAccounts = items.map(item => get(item, 'bankAccount'))
+    const properties = items.map(item => get(item, 'property'))
+    const allValuesUnique = bankAccounts.length === new Set(bankAccounts).size
+        && properties.length === new Set(properties).size
+    return allValuesSet && allValuesUnique
+}
+
 const SbbolImportModal: ISbbolImportModal = ({ open }) => {
     const intl = useIntl()
     const SetupSyncTitle = intl.formatMessage({ id: 'pages.banking.report.accountSetupTitle' })
@@ -38,7 +48,7 @@ const SbbolImportModal: ISbbolImportModal = ({ open }) => {
     const AlertDescription = intl.formatMessage({ id: 'pages.banking.report.alertInfo.description' })
     const BankAccountPlaceholder = intl.formatMessage({ id:'pages.banking.report.chooseBankAccountPlaceholder' })
     const PropertyPlaceholder = intl.formatMessage({ id:'pages.banking.report.choosePropertyPlaceholder' })
-    const FieldIsRequiredMsg = intl.formatMessage({ id: 'FieldIsRequired' })
+    const NotUniqueError = intl.formatMessage({ id: 'pages.banking.report.validation.notUnique' })
     const SaveTitle = intl.formatMessage({ id: 'Save' })
     const AddTitle = intl.formatMessage({ id: 'pages.banking.report.addAnotherBankAccount' })
     const CancelTitle = intl.formatMessage({ id: 'Cancel' })
@@ -54,8 +64,11 @@ const SbbolImportModal: ISbbolImportModal = ({ open }) => {
     const { objs: properties, loading: propertiesLoading } = Property.useObjects({
         where: { organization: { id: get(organization, 'id') } },
     })
+    const { requiredValidator } = useValidations()
     const [form] = Form.useForm()
     const formWatch = Form.useWatch('items', form)
+
+    const [isValid, setIsValid] = useState(false)
 
     useEffect(() => {
         if (!propertiesLoading && !bankAccountsLoading) {
@@ -67,6 +80,14 @@ const SbbolImportModal: ISbbolImportModal = ({ open }) => {
             }
         }
     }, [propertiesLoading, properties, bankAccountsLoading, bankAccounts, id])
+    useEffect(() => {
+        const values = formWatch ? formWatch : []
+        if (values.length) {
+            const validationResult = validateFormItems(values)
+            form.validateFields()
+            setIsValid(validationResult)
+        }
+    }, [formWatch])
 
     const handleRemove = useCallback((remove) => {
         remove(formWatch.length - 1)
@@ -74,6 +95,18 @@ const SbbolImportModal: ISbbolImportModal = ({ open }) => {
     const handleSubmit = useCallback(() => {
         console.log(form.getFieldsValue())
     }, [form])
+    const uniqueItemValidator = useCallback((itemKey: string, index: number) => ({
+        message: NotUniqueError,
+        validator: (_, value) => {
+            const formValues = formWatch ? formWatch : []
+            const selectedValues = formValues.map(item => get(item, itemKey))
+            selectedValues.splice(index, 1)
+            if (selectedValues.includes(value)) {
+                return Promise.reject()
+            }
+            return Promise.resolve()
+        },
+    }), [NotUniqueError, formWatch])
 
     const hasBankAccounts = !bankAccountsLoading && bankAccounts.length
     const formValues = formWatch ? formWatch : []
@@ -82,7 +115,11 @@ const SbbolImportModal: ISbbolImportModal = ({ open }) => {
         <Modal
             title={hasBankAccounts ? SetupSyncTitle : BankAccountNotFoundTitle}
             open={open}
-            footer={hasBankAccounts ? <Button type='primary' onClick={handleSubmit}>{SaveTitle}</Button> : null}
+            footer={
+                hasBankAccounts ?
+                    <Button type='primary' disabled={!isValid} onClick={handleSubmit}>{SaveTitle}</Button>
+                    : null
+            }
         >
             <Row gutter={MODAL_ROW_GUTTER}>
                 <Col span={24}>
@@ -90,10 +127,12 @@ const SbbolImportModal: ISbbolImportModal = ({ open }) => {
                 </Col>
                 <Col span={24} hidden={!hasBankAccounts}>
                     <Typography.Text>
-                        {intl.formatMessage({ id: 'pages.banking.report.importHelpText' }, { isSingular: bankAccounts.length })}
+                        {intl.formatMessage(
+                            { id: 'pages.banking.report.importHelpText' }, { isSingular: bankAccounts.length })
+                        }
                     </Typography.Text>
                 </Col>
-                <Col span={24}>
+                <Col span={24} hidden={!hasBankAccounts}>
                     <Form
                         form={form}
                         initialValues={{ items: [{ property: null, bankAccount: null }] }}
@@ -107,8 +146,10 @@ const SbbolImportModal: ISbbolImportModal = ({ open }) => {
                                                 <Form.Item
                                                     {...restField}
                                                     name={[name, 'property']}
-                                                    rules={[{ required: true, message: FieldIsRequiredMsg }]}
-                                                    shouldUpdate
+                                                    rules={[
+                                                        requiredValidator,
+                                                        uniqueItemValidator('property', index),
+                                                    ]}
                                                 >
                                                     <Select
                                                         placeholder={PropertyPlaceholder}
@@ -126,8 +167,10 @@ const SbbolImportModal: ISbbolImportModal = ({ open }) => {
                                                 <Form.Item
                                                     {...restField}
                                                     name={[name, 'bankAccount']}
-                                                    rules={[{ required: true, message: FieldIsRequiredMsg }]}
-                                                    shouldUpdate
+                                                    rules={[
+                                                        requiredValidator,
+                                                        uniqueItemValidator('bankAccount', index),
+                                                    ]}
                                                 >
                                                     <Select
                                                         placeholder={BankAccountPlaceholder}
@@ -154,10 +197,12 @@ const SbbolImportModal: ISbbolImportModal = ({ open }) => {
                                                     <Typography.Title level={5}>{CancelTitle}</Typography.Title>
                                                 </TextButton>
                                             )}
-                                            <TextButton onClick={add}>
-                                                <PlusCircle size='small' />
-                                                <Typography.Title level={5}>{AddTitle}</Typography.Title>
-                                            </TextButton>
+                                            {formValues.length < bankAccounts.length && (
+                                                <TextButton onClick={add}>
+                                                    <PlusCircle size='small' />
+                                                    <Typography.Title level={5}>{AddTitle}</Typography.Title>
+                                                </TextButton>
+                                            )}
                                         </Space>
                                     </Col>
                                 </Row>
