a59547283b8de3f38b4f2bb521d71502c7a21578
Sitnikov Ivan <sit7602@gmail.com>
2024-04-01 09:54:06 +0500

    fix(condo): DOMA-8679 change log dir

--- run_condo_domain_tests.sh
+++ run_condo_domain_tests.sh
@@ -49,7 +49,6 @@ node -e 'console.log(v8.getHeapStatistics().heap_size_limit/(1024*1024))'
 
 # NOTE(pahaz): Keystone not in dev mode trying to check dist/admin folder
 mkdir -p ./apps/condo/dist/admin
-mkdir -p test_logs
 
 ls
 
